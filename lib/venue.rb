52e132796feb3900552c91f54bcd5af45e540b90
Netia Ingram <netia1128@hotmail.com>
2021-03-24 14:55:01 -0600

    Add iteration 3 to spec file

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    uppercased_patrons = []
+    patrons.each do |patron|
+      uppercased_patrons << patron.upcase
+    end
+    uppercased_patrons
+  end
+
+  def over_capacity?
+    if patrons.count > 4
+      true
+    else
+      false
+    end
+  end
+
 end
